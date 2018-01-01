c8baeca95f9946893baa27aebc47f92e46dba1fc
Asho <angryasho@gmail.com>
2018-01-01 21:34:11 +0200

    Add project files.

---
+++ HappyNewYear/HappyNewYear.cs
@@ -0,0 +1,24 @@
+﻿namespace HappyNewYear
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public static class HappyNewYear
+    {
+        private static int newYear;
+
+        static HappyNewYear()
+        {
+            newYear = 2017;
+        }
+
+        public static int MoveToTheNextYear()
+        {
+            return Interlocked.Increment(ref newYear);
+        }
+    }
+}
